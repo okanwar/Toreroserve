1466bddefb0b9e2a38132361952a19e3ce9297ac
Sat Garcia <elsaturnino@gmail.com>
2016-09-16 16:55:24 -0700

    prettify some comments and add some suggested TODOs

--- toreroserve.c
+++ toreroserve.c
@@ -67,7 +67,8 @@ int createSocketAndListen(int port_num) {
         exit(1);
     }
 
-    /* A server socket is bound to a port, which it will listen on for incoming
+    /* 
+	 * A server socket is bound to a port, which it will listen on for incoming
      * connections.  By default, when a bound socket is closed, the OS waits a
      * couple of minutes before allowing the port to be re-used.  This is
      * inconvenient when you're developing an application, since it means that
@@ -75,7 +76,8 @@ int createSocketAndListen(int port_num) {
      * we can disable the wait time by setting a socket option called
      * SO_REUSEADDR, which tells the OS that we want to be able to immediately
      * re-bind to that same port. See the socket(7) man page ("man 7 socket")
-     * and setsockopt(2) pages for more details about socket options. */
+     * and setsockopt(2) pages for more details about socket options.
+	 */
     int reuse_true = 1;
 
 	int retval; // for checking return values
@@ -88,7 +90,8 @@ int createSocketAndListen(int port_num) {
         exit(1);
     }
 
-    /* Create an address structure.  This is very similar to what we saw on the
+    /*
+	 * Create an address structure.  This is very similar to what we saw on the
      * client side, only this time, we're not telling the OS where to connect,
      * we're telling it to bind to a particular address and port to receive
      * incoming connections.  Like the client side, we must use htons() to put
@@ -96,25 +99,30 @@ int createSocketAndListen(int port_num) {
      * we use a special constant, INADDR_ANY, which tells the OS to bind to all
      * of the system's addresses.  If your machine has multiple network
      * interfaces, and you only wanted to accept connections from one of them,
-     * you could supply the address of the interface you wanted to use here. */
+     * you could supply the address of the interface you wanted to use here.
+	 */
     struct sockaddr_in addr;
     addr.sin_family = AF_INET;
     addr.sin_port = htons(port_num);
     addr.sin_addr.s_addr = INADDR_ANY;
 
-    /* As its name implies, this system call asks the OS to bind the socket to
-     * address and port specified above. */
+    /* 
+	 * As its name implies, this system call asks the OS to bind the socket to
+     * address and port specified above.
+	 */
     retval = bind(sock, (struct sockaddr*)&addr, sizeof(addr));
     if(retval < 0) {
         perror("Error binding to port");
         exit(1);
     }
 
-    /* Now that we've bound to an address and port, we tell the OS that we're
+    /* 
+	 * Now that we've bound to an address and port, we tell the OS that we're
      * ready to start listening for client connections.  This effectively
      * activates the server socket.  BACKLOG (#defined above) tells the OS how
      * much space to reserve for incoming connections that have not yet been
-     * accepted. */
+     * accepted.
+	 */
     retval = listen(sock, BACKLOG);
     if(retval < 0) {
         perror("Error listening for connections");
@@ -131,36 +139,49 @@ int createSocketAndListen(int port_num) {
  */
 void acceptConnections(int server_sock) {
     while(1) {
-        /* Declare a socket for the client connection. */
+        // Declare a socket for the client connection.
         int sock;
 
-        /* Another address structure.  This time, the system will automatically
+        /* 
+		 * Another address structure.  This time, the system will automatically
          * fill it in, when we accept a connection, to tell us where the
-         * connection came from. */
+         * connection came from.
+		 */
         struct sockaddr_in remote_addr;
         unsigned int socklen = sizeof(remote_addr); 
 
-        /* Accept the first waiting connection from the server socket and
+        /* 
+		 * Accept the first waiting connection from the server socket and
          * populate the address information.  The result (sock) is a socket
          * descriptor for the conversation with the newly connected client.  If
          * there are no pending connections in the back log, this function will
          * block indefinitely while waiting for a client connection to be made.
-         * */
+         */
         sock = accept(server_sock, (struct sockaddr*) &remote_addr, &socklen);
         if(sock < 0) {
             perror("Error accepting connection");
             exit(1);
         }
 
-        /* At this point, you have a connected socket (named sock) that you can
-         * use to send() and recv(). */
+        /* 
+		 * At this point, you have a connected socket (named sock) that you can
+         * use to send() and recv().
+		 * TODO: You should send this socket to a separate thread to handle
+		 * rather than doing any sends or recvs here.
+		 */
 
-        /* ALWAYS check the return value of send().  Also, don't hardcode
-         * values.  This is just an example.  Do as I say, not as I do, etc. */
+        /* 
+		 * ALWAYS check the return value of send().  Also, don't hardcode
+         * values.  This is just an example.  Do as I say, not as I do, etc.
+		 * TODO: You may want to reuse the send_or_exit and recv_or_exit
+		 * functions I provided in lab 02's solution.
+		 */
         send(sock, "Howdy, client.\n", 15, 0);
 
-        /* Tell the OS to clean up the resources associated with that client
-         * connection, now that we're done with it. */
+        /* 
+		 * Tell the OS to clean up the resources associated with that client
+         * connection, now that we're done with it.
+		 */
         close(sock);
     }
 }
